16da162355a2a191f9076cad8adb2479e5f41923
Valeria Rodina <valeriarodina@ya.ru>
2016-01-25 00:58:00 +0300

    ispravlen

--- scripts_dml_individ/dml_1.sql
+++ scripts_dml_individ/dml_1.sql
@@ -1,6 +1,9 @@
 connect 'C:\BD\FITNESSCLUB.FDB' user 'SYSDBA' password 'masterkey';
 create view top10clients as
-select first 10 clients.clientname as name, clients.visitnumbers as visitss from clients
-order by clients.visitnumbers desc;
+select first 10 clients.clientname as name, COUNT (visits.dateofvisit) as visitss from visits, clients
+where visits.id_client = clients.id_client group by
+    clients.clientname
+order by visitss desc
+;
 
 
--- tr/tr2.sql
+++ tr/tr2.sql
@@ -3,14 +3,15 @@ CREATE TRIGGER TR2 FOR VISITS
 ACTIVE BEFORE INSERT POSITION 0
 AS
 declare variable bufdatstart date;
+declare variable bufdatend date;
 begin
 
-    for select tickets.validity
+    for select tickets.validity,tickets.finish_date
         from tickets
-        where tickets.id_client = new.id_client into :bufdatstart
+        where tickets.id_client = new.id_client into :bufdatstart,:bufdatend
         do
         begin
-            if(new.dateofvisit<bufdatstart)
+            if((new.dateofvisit<bufdatstart) or (new.dateofvisit > bufdatend))
             then
             begin
             exception notacttick;
