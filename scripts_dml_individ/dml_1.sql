085f271b8db715060d2c0a49f79b0df446604742
Valeria Rodina <valeriarodina@ya.ru>
2016-01-27 10:43:53 +0300

    change - clientname ->> id_client in top10clients

--- scripts_dml_individ/dml_1.sql
+++ scripts_dml_individ/dml_1.sql
@@ -1,8 +1,8 @@
 connect 'C:\BD\FITNESSCLUB.FDB' user 'SYSDBA' password 'masterkey';
 create view top10clients as
-select first 10 clients.clientname as name, COUNT (visits.dateofvisit) as visitss from visits, clients
+select first 10 clients.id_client as number_of_client, COUNT (visits.dateofvisit) as visitss from visits, clients
 where visits.id_client = clients.id_client group by
-    clients.clientname
+    clients.id_client
 order by visitss desc
 ;
 
